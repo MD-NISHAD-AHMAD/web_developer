c2ad0dcebeee46fdbef3fe11875d18b710b3f422
MD-NISHAD-AHMAD <nishadahmadm@gmail.com>
2025-10-29 10:05:04 +0530

    movie api

---
+++ JAVASCRIPT 2.0/Movie_Api/index.js
@@ -0,0 +1,47 @@
+ 
+ const searchMovie = () => {
+  let query = document.getElementById("searchInput").value;
+
+  if(query === ""){
+    alert("Please enter movie name");
+    return;
+  }
+
+  fetch(`https://www.omdbapi.com/?s=${query}&apikey=6b893790`)
+  .then((res) => res.json())
+  .then((data) => {
+    if(data.Response === "False"){
+      document.getElementById("movie").innerHTML = "<h2>No movie found</h2>";
+      return;
+    }
+    Render_UI(data.Search);
+  })
+  .catch((err) => console.log(err));
+};
+
+
+const Render_UI = (movies) => {
+    let mainDiv = document.getElementById("movie");
+    mainDiv.innerHTML = "";
+
+    movies.map((Element) => {
+        let cardDiv = document.createElement('div');
+
+        let img = document.createElement('img');
+        let title = document.createElement('h3');
+        let type = document.createElement('p');
+        let year = document.createElement('p');
+        let imdbId = document.createElement('p'); // ✅ correct variable
+
+        img.src = Element.Poster;
+        title.innerText = Element.Title;
+        type.innerText = "Type: " + Element.Type;
+        year.innerText = "Year: " + Element.Year;
+        imdbId.innerText = "IMDB ID: " + Element.imdbID; // ✅ correct key
+
+        cardDiv.className = "card-div";
+
+        cardDiv.append(img, title, type, year, imdbId);
+        mainDiv.append(cardDiv);
+    });
+};
